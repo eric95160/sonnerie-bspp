bbb29da80f3b022c30199252a0cb943a3f43e4b9
eric95160 <38341053+eric95160@users.noreply.github.com>
2018-04-13 08:29:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,26 +6,3 @@ bot.on('ready', function () {
 })
 
 bot.login('NDM0MTgzODkwNjgxNDYyNzg0.DbGtKg.1WmC3vj94U-LK9lZ6WkG8ZTSXWY')
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.login('434183890681462784');
-
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
