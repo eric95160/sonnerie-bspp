e78339d5884c397293cda7bda3222ebaab7ef5a9
eric95160 <38341053+eric95160@users.noreply.github.com>
2018-04-27 22:03:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,7 @@
-const  Discord  =  require ( ' discord.js ' );
-const  music  =  require ( ' discord.js-music-v11 ' );
-Bot . sur ( ' prêt ' , () => {
-     console . log ( ` [Start] $ { nouvelle  date () } ` );
-});
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
-musique (Bot, {
-	préfixe :  ' - ' ,         // Préfixe de '-'. 
-	global :  false ,       // Files d'attente spécifiques au serveur. 
-	maxQueueSize :  10 ,    // Taille maximale de la file d'attente de 10. 
-	clearInvoker :  true , // Si les autorisations sont applicables, autoriser le bot à supprimer les messages qui l'invoquent (début avec préfixe) 
-    channel :  ' music '     // Nom du canal vocal à rejoindre . S'il est omis, il rejoindra à la place le canal vocal de l'utilisateur.
-});
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
 bot.login('NDM0MTgzODkwNjgxNDYyNzg0.DbGtKg.1WmC3vj94U-LK9lZ6WkG8ZTSXWY')
