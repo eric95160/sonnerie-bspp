63b32ea0776f0ce7dee5ce613f424526a6a27e54
eric95160 <38341053+eric95160@users.noreply.github.com>
2018-04-27 22:12:11 +0200

    Delete index.js

--- .gitignore/index.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-
-bot.on('ready', function () {
-  console.log("Je suis connecté !")
-})
-bot.login('NDM0MTgzODkwNjgxNDYyNzg0.DbGtKg.1WmC3vj94U-LK9lZ6WkG8ZTSXWY')
